dbaae0416d98cb793fb6d13afb9d62f25b873f5e
masterthuku <peterthuku706@gmail.com>
2022-11-15 09:03:43 +0300

    Add project files.

---
+++ grading system/grading system.cpp
@@ -0,0 +1,32 @@
+// grading system.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* if else statement */
+
+int main()
+{
+	int marks;
+	marks = 39;
+
+	if (marks >= 70)
+		printf("GRADE A");
+
+	else if (marks >= 60)
+
+		printf("GRADE B ");
+
+	else if (marks >= 50)
+		printf("GRADE C");
+
+	else if (marks >= 40)
+		printf("GRADE D");
+
+	else
+		printf("F");
+
+	return 0;
+
+}
